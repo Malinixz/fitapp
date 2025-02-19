7797d590a738d7f4dce4ad3264a8e363de6257ca
Miguel <miguel.malini5963@gmail.com>
2025-02-19 19:26:54 -0300

    Separação do front

--- front-new/src/utils/Dates.tsx
+++
@@ -1,7 +0,0 @@
-export function convertToDate(dateString : string): Date {
-    // Divida a string no formato DD/MM/YYYY
-    const [day, month, year] = dateString.split('/');
-
-    // Crie um objeto Date no formato YYYY-MM-DD
-    return new Date(`${year}-${month}-${day}`);
-}
